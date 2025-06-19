fa8dd39f6d6618e932c738c809c1cef7697756eb
Pau Bibiloni Martínez <psbibiloni@gmail.com>
2025-06-19 02:22:32 +0200

    ahora si, falta implementar la opcion genera_nonograma

--- nonograma.pl
+++ nonograma.pl
@@ -9,16 +9,16 @@
 %% Ejemplo de uso del programa:
 %%
 %% ?- nonograma(
-%%       [[2], [5], [1,1], [5], [3]],
-%%       [[1,1], [3], [2], [5], [2]],
+%%       [[5], [5], [1,1], [3], [2]],
+%%       [[2,2], [5], [2,1], [2], [3]],
 %%       Caselles),
 %%    imprimir_tablero(Caselles).
 %%
-%% Caselles = [[□, ■, ■, □, □],
+%% Caselles = [[■, ■, ■, ■, ■],
 %%             [■, ■, ■, ■, ■],
-%%             [■, □, ■, □, □],
-%%             [■, ■, ■, ■, ■],
-%%             [□, ■, ■, ■, □]]
+%%             [□, ■, □, □, ■],
+%%             [■, ■, ■, □, □],
+%%             [■, ■, □, □, □]]
 %%
 %% También puedes generar uno aleatorio:
 %%
@@ -28,113 +28,207 @@
 %%
 %% =============================================================================
 
-%% =============================================================================
-%% Predicado principal: resuelve el nonograma dadas las pistas de filas y columnas
-%% =============================================================================
-nonograma(Filas, Columnas, Caselles) :-
-    length(Columnas, M),
-    genera_tauler(Filas, M, Caselles),
-    transposta(Caselles, Cols),
-    valida_columnes(Columnas, Cols).
-%% =============================================================================
-%% Genera una matriz Caselles a partir de las pistas de fila
-%% =============================================================================
-genera_tauler([], _, []).
-genera_tauler([P|Ps], M, [F|Fs]) :-
-    genera_linea(P, M, F),
-    genera_tauler(Ps, M, Fs).
 
 %% =============================================================================
-%% Verifica que cada columna obtenida cumple exactamente sus pistas
+%% Predicat principal
 %% =============================================================================
-valida_columnes([], []).
-valida_columnes([P|Ps], [C|Cs]) :-
-    obtenir_pistes(C, P),
-    valida_columnes(Ps, Cs).
+nonograma(PistasFilas, PistasColumnas, Caselles) :-
+    longitud(PistasFilas, N),
+    longitud(PistasColumnas, M),
+    crear_filas(N, M, Caselles),
+    restringir_lineas(PistasFilas, Caselles),
+    transpose(Caselles, Transp),
+    restringir_lineas(PistasColumnas, Transp),
+    etiquetar_lineas(Caselles).
 
 %% =============================================================================
-%% Genera una línea binaria que satisface una pista dada, con backtracking completo
+%% Crear matriu de N files de M columnes amb variables lliures
 %% =============================================================================
-genera_linea(Pistas, Long, Linea) :-
-    sum_list(Pistas, Suma),
-    length(Pistas, L),
-    MinLen is Suma + L - 1,
-    PadMax is Long - MinLen,
-    PadMax >= 0,
-    entre(0, PadMax, Pad),
-    rellenar_ceros(Pad, Inici),
-    construeix_blocs(Pistas, Cos),
-    append(Inici, Cos, Temp),
-    length(Temp, LTemp),
-    PadFinal is Long - LTemp,
-    rellenar_ceros(PadFinal, Final),
-    append(Temp, Final, Linea).
+crear_filas(0, _, []).
+crear_filas(N, M, [Fila|Resto]) :-
+    N > 0,
+    crear_fila(M, Fila),
+    N1 is N - 1,
+    crear_filas(N1, M, Resto).
+
+crear_fila(0, []).
+crear_fila(M, [_|Resto]) :-
+    M > 0,
+    M1 is M - 1,
+    crear_fila(M1, Resto).
 
 %% =============================================================================
-%% Construye los bloques de 1s separados por ceros según la pista
+%% Aplicar restriccions de les pistes sobre cada línia
 %% =============================================================================
-construeix_blocs([], []).
-construeix_blocs([X], Bloc) :-
-    rellenar_unos(X, Bloc).
-construeix_blocs([X|Xs], Bloc) :-
-    rellenar_unos(X, Uns),
-    construeix_blocs(Xs, R),
-    append(Uns, [0|R], Bloc).
+restringir_lineas([], []).
+restringir_lineas([P|Ps], [L|Ls]) :-
+    restringir_linea(P, L),
+    restringir_lineas(Ps, Ls).
+
+restringir_linea(Pistas, Linea) :-
+    longitud(Linea, N),
+    generar_linea(Pistas, N, Linea),
+    etiquetar_linea(Linea).
 
 %% =============================================================================
-%% Extrae las pistas (bloques de 1s consecutivos) de una línea binaria
+%% Generar línia binària que compleix les pistes
 %% =============================================================================
+generar_linea([], Longitud, Linea) :-
+    ceros(Longitud, Linea).
+
+generar_linea([X|Xs], Longitud, Linea) :-
+    suma(Xs, SumaRestos),
+    longitud(Xs, NumRestos),
+    MinLong is X + SumaRestos + NumRestos,
+    MaxPadding is Longitud - MinLong,
+    MaxPadding >= 0,
+    entre(0, MaxPadding, Padding),
+    ceros(Padding, Prefijo),
+    unos(X, Bloque),
+    RestoLong is Longitud - Padding - X,
+    generar_sufijo(Xs, RestoLong, Suffix),
+    append(Prefijo, Bloque, Parte),
+    append(Parte, Suffix, Linea),
+    longitud(Linea, Longitud).
+
+generar_sufijo([], RestoLong, Suffix) :-
+    ceros(RestoLong, Suffix).
+
+generar_sufijo([Y|Ys], RestoLong, [0|SubLinea]) :-
+    RestoLong1 is RestoLong - 1,
+    RestoLong1 >= 0,
+    generar_linea([Y|Ys], RestoLong1, SubLinea).
+
+%% =============================================================================
+%% Utilitats bàsiques
+%% =============================================================================
+ceros(0, []).
+ceros(N, [0|R]) :-
+    N > 0,
+    N1 is N - 1,
+    ceros(N1, R).
+
+unos(0, []).
+unos(N, [1|R]) :-
+    N > 0,
+    N1 is N - 1,
+    unos(N1, R).
+
+longitud([], 0).
+longitud([_|Xs], N) :-
+    longitud(Xs, M),
+    N is M + 1.
+
+suma([], 0).
+suma([X|Xs], R) :-
+    suma(Xs, S),
+    R is X + S.
+
+entre(X, _, X).
+entre(Min, Max, X) :-
+    Min < Max,
+    Min1 is Min + 1,
+    entre(Min1, Max, X).
+
+%% =============================================================================
+%% Etiquetar línia amb 0 o 1 per backtracking
+%% =============================================================================
+etiquetar_linea([]).
+etiquetar_linea([X|Xs]) :-
+    (X = 0 ; X = 1),
+    etiquetar_linea(Xs).
+
+etiquetar_lineas([]).
+etiquetar_lineas([Fila|Resto]) :-
+    etiquetar_linea(Fila),
+    etiquetar_lineas(Resto).
+
+%% =============================================================================
+%% Agrupar valors consecutius i extreure pistes
+%% =============================================================================
+agrupa([], []).
+agrupa([X|Xs], [[X|Grupo]|Resto]) :-
+    iguals(X, Xs, Grupo, RestoXs),
+    agrupa(RestoXs, Resto).
+
+iguals(_, [], [], []).
+iguals(X, [X|Xs], [X|R], Resto) :-
+    iguals(X, Xs, R, Resto).
+iguals(X, [Y|Ys], [], [Y|Ys]) :-
+    diferent(X, Y).
+
+diferent(0, 1).
+diferent(1, 0).
+
+es_bloque_pleno([1|_]).
+
+filtrar([], []).
+filtrar([G|Gs], [G|Rs]) :-
+    es_bloque_pleno(G),
+    filtrar(Gs, Rs).
+filtrar([G|Gs], Rs) :-
+    no_bloque(G),
+    filtrar(Gs, Rs).
+
+no_bloque([0|_]).
+
+longitudes([], []).
+longitudes([L|Ls], [N|Ns]) :-
+    longitud(L, N),
+    longitudes(Ls, Ns).
+
+%% =============================================================================
+%% Generador aleatori de nonogrames
+%% =============================================================================
+genera_nonograma(N, M, PistasFilas, PistasColumnas, Caselles) :-
+    crear_filas(N, M, Caselles),
+    aleatoritzar_tauler(Caselles),
+    obtenir_pistes(Caselles, PistasFilas),
+    transpose(Caselles, Transp),
+    obtenir_pistes(Transp, PistasColumnas).
+
+aleatoritzar_tauler([]).
+aleatoritzar_tauler([F|Fs]) :-
+    aleatoritzar_fila(F),
+    aleatoritzar_tauler(Fs).
+
+aleatoritzar_fila([]).
+aleatoritzar_fila([X|Xs]) :-
+    random(0.0, 1.0, R),
+    assigna_valor(R, X),
+    aleatoritzar_fila(Xs).
+
+assigna_valor(R, 1) :- R < 0.7.
+assigna_valor(R, 0) :- R >= 0.7.
+
 obtenir_pistes([], []).
-obtenir_pistes([0|R], Pistes) :- obtenir_pistes(R, Pistes).
-obtenir_pistes([1|R], [N|Ps]) :- conta_uns([1|R], N, Resto), obtenir_pistes(Resto, Ps).
+obtenir_pistes([F|Fs], [P|Ps]) :-
+    agrupa(F, Grups),
+    filtrar(Grups, Plens),
+    longitudes(Plens, P),
+    obtenir_pistes(Fs, Ps).
 
 %% =============================================================================
-%% Cuenta la cantidad de 1s consecutivos desde el inicio
+%% Transposar matriu (implementació pròpia)
 %% =============================================================================
-conta_uns([1|R], N, Rest) :- conta_uns(R, N1, Rest), N is N1 + 1.
-conta_uns([0|R], 0, [0|R]).
-conta_uns([], 0, []).
+transpose([], []).
+transpose([[]|_], []).
+transpose(M, [F|Fs]) :-
+    columna(M, F, R),
+    transpose(R, Fs).
+
+columna([], [], []).
+columna([[X|Xs]|Resto], [X|Primers], [Xs|Restants]) :-
+    columna(Resto, Primers, Restants).
 
 %% =============================================================================
-%% Genera una lista de ceros de longitud N
-%% =============================================================================
-rellenar_ceros(0, []).
-rellenar_ceros(N, [0|R]) :- N > 0, N1 is N - 1, rellenar_ceros(N1, R).
-
-%% =============================================================================
-%% Genera una lista de unos de longitud N
-%% =============================================================================
-rellenar_unos(0, []).
-rellenar_unos(N, [1|R]) :- N > 0, N1 is N - 1, rellenar_unos(N1, R).
-
-%% =============================================================================
-%% Generador de números entre A y B (inclusivo), usado para padding
-%% =============================================================================
-entre(A, B, A) :- A =< B.
-entre(A, B, R) :- A < B, A1 is A + 1, entre(A1, B, R).
-
-%% =============================================================================
-%% Transpone una matriz (lista de listas)
-%% =============================================================================
-transposta([], []).
-transposta([[]|_], []).
-transposta(M, [F|Fs]) :- primera_columna(M, F, R), transposta(R, Fs).
-
-%% =============================================================================
-%% Extrae la primera columna de una matriz y devuelve el resto
-%% =============================================================================
-primera_columna([], [], []).
-primera_columna([[X|Xs]|Rs], [X|R1], [Xs|R2]) :- primera_columna(Rs, R1, R2).
-
-%% =============================================================================
-%% Imprime una matriz binaria con símbolos gráficos
+%% Impressió del tauler
 %% =============================================================================
 imprimir_tablero([]).
-imprimir_tablero([F|R]) :- imprimir_fila(F), nl, imprimir_tablero(R).
+imprimir_tablero([Fila|Resto]) :-
+    imprimir_fila(Fila), nl,
+    imprimir_tablero(Resto).
 
-%% =============================================================================
-%% Imprime una fila de la matriz con símbolos gráficos
-%% =============================================================================
 imprimir_fila([]).
-imprimir_fila([1|R]) :- write('■ '), imprimir_fila(R).
-imprimir_fila([0|R]) :- write('□ '), imprimir_fila(R).
+imprimir_fila([1|Xs]) :- write('■ '), imprimir_fila(Xs).
+imprimir_fila([0|Xs]) :- write('□ '), imprimir_fila(Xs).
